222a8223336f147c72765b587d28400ddb266de4
Max2000s <max.schoellkopf@gmx.de>
2024-05-26 13:10:50 +0200

    updated functions for persistence file storage

--- task_time_tracker/src/persistence/file_storage.rs
+++ task_time_tracker/src/persistence/file_storage.rs
@@ -5,20 +5,30 @@ use std::io::{Read, Write};
 
 const FILE_PATH: &str = "tasks.json";
 
-pub fn save_task(task: &Task) {
-    let mut tasks = load_tasks();
-    tasks.push(task.clone());
-    let json = serde_json::to_string(&tasks).unwrap();
+pub fn save_task(tasks: &[Task]) {
+    let json = serde_json::to_string(tasks).unwrap();
     let mut file = File::create(FILE_PATH).unwrap();
     file.write_all(json.as_bytes()).unwrap();
 }
 
+pub fn save_tasks(tasks: &[Task]) -> Result<(), std::io::Error> {
+    let json = serde_json::to_string(tasks).unwrap();
+    let mut file = File::create(FILE_PATH)?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
+}
+
 pub fn load_tasks() -> Vec<Task> {
-    let mut file = File::open(FILE_PATH).unwrap_or(File::create(FILE_PATH).unwrap());
+    let mut file = match File::open(FILE_PATH) {
+        Ok(file) => file,
+        Err(_) => return Vec::new(), // Return an empty vector if file does not exist
+    };
+
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
     if content.is_empty() {
-        return Vec::new();
+        Vec::new()
+    } else {
+        serde_json::from_str(&content).unwrap()
     }
-    serde_json::from_str(&content).unwrap()
 }
