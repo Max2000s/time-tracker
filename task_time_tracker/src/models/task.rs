d116e7818eb77ce57ed1533629a1870f598076af
Max2000s <max.schoellkopf@gmx.de>
2024-05-26 13:08:31 +0200

    updated data structure for tasks

--- task_time_tracker/src/models/task.rs
+++ task_time_tracker/src/models/task.rs
@@ -1,16 +1,16 @@
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Task {
     pub id: u32,
     pub name: String,
+    pub active: bool,
     pub start_time: Option<DateTime<Utc>>,
-    pub accumulated_time: Duration,
     pub history: Vec<TimeEntry>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TimeEntry {
     pub start: DateTime<Utc>,
     pub end: DateTime<Utc>,
