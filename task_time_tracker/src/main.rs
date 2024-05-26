44917eaffd681d9fb66f180541813cde86f43790
Max2000s <max.schoellkopf@gmx.de>
2024-05-26 16:13:02 +0200

    updated used commands

--- task_time_tracker/src/main.rs
+++ task_time_tracker/src/main.rs
@@ -21,55 +21,41 @@ fn main() {
                 .about("Deletes the task by its name")
                 .arg(Arg::with_name("task_name").required(true)),
         )
-        /*.subcommand(
-            SubCommand::with_name("continue")
-                .about("Continue a task")
-                .arg(Arg::with_name("task_name").required(true)),
-        )
-         .subcommand(
-            SubCommand::with_name("break")
-                .about("Take a break")
-                .arg(Arg::with_name("break_name").required(true)),
-        )
-        .subcommand(SubCommand::with_name("end_break").about("End the current break"))
-        .subcommand(SubCommand::with_name("list").about("List all tasks"))
         .subcommand(
-            SubCommand::with_name("summary")
-                .about("Show task summary")
-                .arg(Arg::with_name("task_name").required(false)),
+            SubCommand::with_name("report")
+                .about("Generate a report of time spent on tasks")
+                .arg(
+                    Arg::with_name("period")
+                        .short('p')
+                        .long("period")
+                        .takes_value(true)
+                        .possible_values(&["day", "week", "month", "all"])
+                        .default_value("all")
+                        .help("Specify the period for the report"),
+                )
+                .arg(
+                    Arg::with_name("export")
+                        .short('e')
+                        .long("export")
+                        .takes_value(true)
+                        .help("Specify a file to export the report"),
+                ),
         )
-        .subcommand(
-            SubCommand::with_name("export")
-                .about("Export data")
-                .arg(Arg::with_name("format").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("import")
-                .about("Import data")
-                .arg(Arg::with_name("file_path").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("filter")
-                .about("Filter tasks by time period")
-                .arg(Arg::with_name("period").required(true)),
-        )*/
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("start") {
         let task_name = matches.value_of("task_name").unwrap();
         commands::start_task::execute(task_name);
-    }
-
-    if matches.subcommand_matches("stop").is_some() {
+    } else if matches.subcommand_matches("stop").is_some() {
         commands::stop_task::execute();
-    }
-
-    if matches.subcommand_matches("list").is_some() {
+    } else if matches.subcommand_matches("list").is_some() {
         commands::list::execute();
-    }
-
-    if let Some(matches) = matches.subcommand_matches("delete") {
+    } else if let Some(matches) = matches.subcommand_matches("delete") {
         let task_name = matches.value_of("task_name").unwrap();
         commands::delete_task::execute(task_name);
+    } else if let Some(matches) = matches.subcommand_matches("report") {
+        let period = matches.value_of("period").unwrap();
+        let export_file = matches.value_of("export");
+        commands::report::execute(period, export_file);
     }
 }
