77c15e8f3a0dbb072cf6eb0b338b1eb60e381b12
Max2000s <max.schoellkopf@gmx.de>
2024-05-26 15:01:50 +0200

    updated the used commands

--- task_time_tracker/src/main.rs
+++ task_time_tracker/src/main.rs
@@ -1,6 +1,4 @@
-
-use clap::{Arg, App, SubCommand};
-use chrono::Utc;
+use clap::{App, Arg, SubCommand};
 
 mod commands;
 mod models;
@@ -9,37 +7,69 @@ mod persistence;
 fn main() {
     let matches = App::new("Task Time Tracker")
         .version("1.0")
-        .author("Your Name <your.email@example.com>")
+        .author("Maximilian Schoellkopf")
         .about("Tracks time spent on tasks")
-        .subcommand(SubCommand::with_name("start")
-            .about("Start a new task")
-            .arg(Arg::with_name("task_name").required(true)))
+        .subcommand(
+            SubCommand::with_name("start")
+                .about("Start a new task")
+                .arg(Arg::with_name("task_name").required(true)),
+        )
         .subcommand(SubCommand::with_name("stop").about("Stop the current task"))
-        .subcommand(SubCommand::with_name("continue")
-            .about("Continue a task")
-            .arg(Arg::with_name("task_name").required(true)))
-        .subcommand(SubCommand::with_name("break")
-            .about("Take a break")
-            .arg(Arg::with_name("break_name").required(true)))
+        .subcommand(SubCommand::with_name("list").about("List all tasks with some information"))
+        .subcommand(
+            SubCommand::with_name("delete")
+                .about("Deletes the task by its name")
+                .arg(Arg::with_name("task_name").required(true)),
+        )
+        /*.subcommand(
+            SubCommand::with_name("continue")
+                .about("Continue a task")
+                .arg(Arg::with_name("task_name").required(true)),
+        )
+         .subcommand(
+            SubCommand::with_name("break")
+                .about("Take a break")
+                .arg(Arg::with_name("break_name").required(true)),
+        )
         .subcommand(SubCommand::with_name("end_break").about("End the current break"))
         .subcommand(SubCommand::with_name("list").about("List all tasks"))
-        .subcommand(SubCommand::with_name("summary")
-            .about("Show task summary")
-            .arg(Arg::with_name("task_name").required(false)))
-        .subcommand(SubCommand::with_name("export")
-            .about("Export data")
-            .arg(Arg::with_name("format").required(true)))
-        .subcommand(SubCommand::with_name("import")
-            .about("Import data")
-            .arg(Arg::with_name("file_path").required(true)))
-        .subcommand(SubCommand::with_name("filter")
-            .about("Filter tasks by time period")
-            .arg(Arg::with_name("period").required(true)))
+        .subcommand(
+            SubCommand::with_name("summary")
+                .about("Show task summary")
+                .arg(Arg::with_name("task_name").required(false)),
+        )
+        .subcommand(
+            SubCommand::with_name("export")
+                .about("Export data")
+                .arg(Arg::with_name("format").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("import")
+                .about("Import data")
+                .arg(Arg::with_name("file_path").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("filter")
+                .about("Filter tasks by time period")
+                .arg(Arg::with_name("period").required(true)),
+        )*/
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("start") {
         let task_name = matches.value_of("task_name").unwrap();
-        commands::start::execute(task_name);
+        commands::start_task::execute(task_name);
     }
-    // Handle other commands similarly...
+
+    if matches.subcommand_matches("stop").is_some() {
+        commands::stop_task::execute();
+    }
+
+    if matches.subcommand_matches("list").is_some() {
+        commands::list::execute();
+    }
+
+    if let Some(matches) = matches.subcommand_matches("delete") {
+        let task_name = matches.value_of("task_name").unwrap();
+        commands::delete_task::execute(task_name);
+    }
 }
