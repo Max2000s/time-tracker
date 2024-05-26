e38bdd075f24303af4f805a802aaad207990319e
Max2000s <max.schoellkopf@gmx.de>
2024-05-26 12:23:57 +0200

    implemented the command for stopping a task

---
+++ task_time_tracker/src/commands/stop_task.rs
@@ -0,0 +1,25 @@
+// src/commands/stop.rs
+use crate::{
+    models::task::TimeEntry,
+    persistence::file_storage::{load_tasks, save_task},
+};
+use chrono::Utc;
+
+pub fn execute() {
+    let mut tasks = load_tasks();
+    if let Some(task) = tasks.iter_mut().find(|t| t.active == true) {
+        println!("Stopped task: {}", task.name);
+
+        let entry: TimeEntry = TimeEntry {
+            start: task.start_time.unwrap(),
+            end: Utc::now(),
+        };
+
+        task.history.push(entry);
+        task.start_time = None;
+        task.active = false;
+        save_task(&tasks);
+    } else {
+        println!("No task is currently running.");
+    }
+}
